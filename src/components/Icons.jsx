d2af75ea1a5524be62f3eeb0fa9e5b929ac99135
mish-nayeem <mish.nayeem@gmail.com>
2026-09-13 02:53:53 +0600

    Add IconLock component to Icons.jsx

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -154,3 +154,12 @@ export function IconClock(props) {
     </svg>
   )
 }
+
+export function IconLock(props) {
+  return (
+    <svg {...base} {...props}>
+      <rect x="4" y="10" width="16" height="11" rx="2" />
+      <path d="M8 10V7a4 4 0 0 1 8 0v3" />
+    </svg>
+  )
+}
